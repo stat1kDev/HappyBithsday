4a24e16fb512823e042a324890e3cfdaf8879db0
Yaroslav Boboshko <wardwarf567765@gmail.com>
2017-10-09 19:35:55 +0300

    changed strings file

---
+++ app/src/main/java/com/stat1k/happybirthsday/MainActivity.java
@@ -0,0 +1,13 @@
+package com.stat1k.happybirthsday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
